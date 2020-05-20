94be76fa41e1872415da3fac0bc37e33b1533406
Sanja <sanja1mandic@gmail.com>
2020-05-20 23:49:23 +0300

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+
+class BlogListView(ListView):
+    
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model=Post
+
+    template_name='post_detail.html'
